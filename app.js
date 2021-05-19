8f38944d1f69b8ee70e0123413d643b06c7d6067
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-19 15:18:18 -0400

    bringing in express, initializing express app, atarting server

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const morgan = require('morgan');
+//bring in express
 const express = require("express");
 
+
+// const morgan = require("morgan");
+
+
+//initialize express app
 const app = express();
 
+
 app.listen(3000, function () {
-    console.log("Server started in port 3000");
-  });
+	console.log("Server started in port 3000");
+});
