0bad64265da9f89c96371edab46b96ee3bbec837
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-19 15:24:32 -0400

    Morgan added for logging

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 //bring in express
 const express = require("express");
 
+//bring in Morgan for logging
+const morgan = require("morgan");
 
-// const morgan = require("morgan");
 
 
 //initialize express app
 const app = express();
 
+//use logger    dev = development  only shows in development
+app.use(morgan("dev"))
 
+
+
+//start express server
 app.listen(3000, function () {
 	console.log("Server started in port 3000");
 });
