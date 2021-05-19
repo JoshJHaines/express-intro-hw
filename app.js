3f5041c3669e7deb00e093f58245185bc5ac4f2b
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-19 16:42:58 -0400

    adding views

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 //bring in Morgan for logging
 const morgan = require("morgan");
 
-
+//bring in path
+const path = require("path")
 
 //initialize express app
 const app = express();
@@ -14,6 +15,8 @@ app.use(morgan("dev"))
 
 //use express json
 app.use(express.json())
+
+app.set("views", path.join(_dirname, "views"))
 
 //start express server
 app.listen(3000, function () {
