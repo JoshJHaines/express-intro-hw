f647f750bd0aed1f149172df5bd3b76fb2b4afc1
JoshuaJHaines <joshjhaines3@gmail.com>
2021-05-19 15:13:23 -0400

    forgot to install express... now instaleld

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const morgan = require('morgan');
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, function () {
+    console.log("Server started in port 3000");
+  });
